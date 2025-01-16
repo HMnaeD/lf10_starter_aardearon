5b03135bd7094abbc562fe58d51d219bb47458e3
AaronHepta <aaron.pieperjohanns@heptacom.de>
2025-01-16 11:20:33 +0100

    SMFS-37 Add anchor for main menu access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {HeaderComponent} from "./header/header.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {ImprintComponent} from "./imprint/imprint.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent, HeaderComponent],
+  imports: [CommonModule, EmployeeListComponent, HeaderComponent, LandingPageComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
