c76cc864887d9c2a4c26d7a0eb02322631e488e4
AaronHepta <aaron.pieperjohanns@heptacom.de>
2025-01-16 09:26:36 +0100

    SMFS-32 Add profile picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
+import {HeaderComponent} from "./header/header.component";
+import {LandingPageComponent} from "./landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent],
+  imports: [CommonModule, EmployeeListComponent, HeaderComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
