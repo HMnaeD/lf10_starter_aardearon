e94329b57f1981936b9362445c477a3664d0726e
Ronny Angel Osca Monroy <ronny.osca@heptacom.de>
2025-01-22 13:21:14 +0100

    SMFS-47/48 Add function to create and delete qualifications

--- src/app/components/qualification-list/modals/delete-modal/delete-modal.component.ts
+++ src/app/components/qualification-list/modals/delete-modal/delete-modal.component.ts
@@ -1,5 +1,4 @@
 import {Component, Input} from '@angular/core';
-import {Qualification} from "../../../../model/qualification";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Component({
@@ -18,7 +17,7 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
             <p class="text-break">Sie sind dabei Qualifikation X zu löschen. Sind Sie sicher, das Sie das möchten?</p>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" (click)="delete()">Löschen</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" (click)="delete()">Löschen</button>
             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Abbrechen</button>
           </div>
         </div>
@@ -28,17 +27,26 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
   styleUrl: './delete-modal.component.css'
 })
 export class DeleteModalComponent {
-  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3MzcwMzE3ODYsImlhdCI6MTczNzAyODE4NiwianRpIjoiYzdjZDExOTQtN2ZhZi00YzQzLWJlZGUtMmVkNGMzZDA0OWQ4IiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiJhYmEzYzllNS01ZTU4LTQxYjMtYjg2Ny0xYTkyM2I0MmYxNjQiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsicHJvZHVjdF9vd25lciIsIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.VwppuMRFrHRkyBLmCieZ95YiutDwrFKVHvQ01SCm_8rGwPQ1_OHCPy0pz22n61iQc2iFU6ThhTFqeAjV5hT6Ecb5A0MMaU0n8Zpds_7dz02LYbO8eysU5LtjYjk-z85EzzugmJ6xJP24upQ9W2Q481TYo6ozJuvPLK-HfbZNvozcMAkZd9kjl0A5zVMHhcKvx2KaV5nkHoalkkzzsj9zBfMeIGI6FBnB9oio7pgvRih5i8ssbzj2i3cnn2vLbHAW-3fYT-gBiUOewZBT1w82_B096Rl1MvAduHCy5hhD9thJLDurK6pajzLiLvenBogcMangnNkA751nUV1Plk9xrA';
+  bearer = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIzUFQ0dldiNno5MnlQWk1EWnBqT1U0RjFVN0lwNi1ELUlqQWVGczJPbGU0In0.eyJleHAiOjE3Mzc1NDk4NTAsImlhdCI6MTczNzU0NjI1MCwianRpIjoiM2JkN2FlZDAtZWE3NS00YjQ5LThlNWEtMTA1ZGZlM2RjMTZjIiwiaXNzIjoiaHR0cHM6Ly9rZXljbG9hay5zenV0LmRldi9hdXRoL3JlYWxtcy9zenV0IiwiYXVkIjoiYWNjb3VudCIsInN1YiI6IjU1NDZjZDIxLTk4NTQtNDMyZi1hNDY3LTRkZTNlZWRmNTg4OSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImVtcGxveWVlLW1hbmFnZW1lbnQtc2VydmljZSIsInNlc3Npb25fc3RhdGUiOiI2MGZhODhjNC1lMTM5LTRjNjYtYWNkNy0wMDcyYjAzM2FkMzIiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHA6Ly9sb2NhbGhvc3Q6NDIwMCJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsicHJvZHVjdF9vd25lciIsIm9mZmxpbmVfYWNjZXNzIiwiZGVmYXVsdC1yb2xlcy1zenV0IiwidW1hX2F1dGhvcml6YXRpb24iLCJ1c2VyIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJlbWFpbCBwcm9maWxlIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInByZWZlcnJlZF91c2VybmFtZSI6InVzZXIifQ.gPgoXzJSa2MWLkN5kabRL1ze8ZmVX7zGGOjzeTzN_13eymCABCQmYFC1G5Hdh2k2fKfhUwpuOTOj9GydplRudfT8lsDQPWmuJJR2FWD5xRNqfUiyLbREs-5mov0fRpM980d3sNYF4v1WGPYyE0a7NRnJk1pODJ0nLEoebEUGVti2JfUt0TGLDvVs1L6Boz76aM47XB1ga5rfUJXa8O1rBGmJHccDqxB2fEfWq9fiev5TtmYO_PFfe94edr1KBNkN3yUiksD_GyxHOX3S6xh2MyNi3kmpxnF61eLO00iuD5hZK-SX8-aPTQNJepc9-a5A3khrfv_kh0f2-ONq6s_8Sw';
   @Input() data!: number | undefined;
 
   constructor(private http: HttpClient) {
   }
 
   delete() {
-    console.log(this.data);
-    this.http.delete<Qualification>(`http://localhost:8089/qualifications/${this.data}`, {
+    return this.http.delete(`http://localhost:8089/qualifications/${this.data}`, {
       headers: new HttpHeaders()
+        .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${this.bearer}`)
+    }).subscribe({
+      next: (response) => {
+        console.log('Erfolgreich gespeichert:', response);
+        location.reload();
+      },
+      error: (err) => {
+        console.error('Fehler beim Speichern:', err);
+      }
     });
+    //console.log(this.data);
   }
 }
